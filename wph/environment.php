d2229ce8cf16d64115a632e6d93f24ed65a250cd
playstore9cs <playstore9cs@gmail.com>
2021-02-20 06:51:41 +0800

    layout

---
+++ wph/environment.php
@@ -0,0 +1,3 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+$environment_variable = '{"themes":{"1":{"themes_url":"http:\/\/playstore9.net\/wp-content\/themes","use_child_theme":false,"main":{"folder_name":"ceomax","mapped_name":""}}},"allowed_paths":["\/www\/wwwroot\/playstore9.net\/wp-content\/themes"],"wordpress_directory":"","site_relative_path":"\/"}'; ?>
